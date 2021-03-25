b70c9d6c410c4ca93856608244b78513a0aa109f
yanno8 <yannesonwafomekong@yahoo.com>
2021-03-25 09:52:49 +0100

    my restaurant project

--- app.js
+++ app.js
@@ -1,9 +1,41 @@
 var express = require('express'),
     app = express(),
     http = require('http').Server(app),
+    bcrypt = require('bcrypt')
+    mysql = require('mysql'),
     io = require('socket.io')(http);
 
+const users = []
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database : "restaurant"
+});
+
+db.connect(function(err) {
+  if (err) throw err;
+  console.log("Connecté à la base de données!");
+});
+
+app.use(express.urlencoded({ extended: false}))
 app.use("/", express.static(__dirname + "/public"));
+
+app.post('/pages/admin/login.html', async function(req, res) {
+  try{
+    const hashedPassword = bcrypt.hash(req.body.password, 10)
+    users.push({
+      id: Date.now().toString(),
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword
+    })
+  } catch {
+    res.redirect('/pages/admin/admin.html')
+  }
+  console.log(users);
+})
 
   // ajouter et supprimer un socket.id de la sauvegarde apres une nouvelle connexion
 
